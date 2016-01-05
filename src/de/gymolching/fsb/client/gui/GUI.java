4c9bd4d9181fe43dc1f386238e780429623ca993
Simon Schäffner <simon.schaeffner@googlemail.com>
2016-01-06 00:53:33 +0100

    adds META-INF

--- src/de/gymolching/fsb/client/gui/GUI.java
+++ src/de/gymolching/fsb/client/gui/GUI.java
@@ -7,12 +7,6 @@ import javax.swing.*;
 
 public class GUI {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new MainFrame("Flugsimulator");
-			}
-		});
-
+		SwingUtilities.invokeLater(() -> new MainFrame("Flugsimulator"));
 	}
 }
